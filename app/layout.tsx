c335e0949caf2fe66eb1ee299bfba6b2b68936b8
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-27 01:56:56 +0300

    Create Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
