10a93893deb16d7189ffbaa50c11243cb477e0dc
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-27 19:12:22 +0300

    Add Billboard to Home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <div>Hello World!</div>;
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export default async function Home() {
+  const billboard = await getBillboard(process.env.BILLBOARD_ID!);
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 }
