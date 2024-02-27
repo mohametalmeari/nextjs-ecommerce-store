c00fd9d807cd41cfa183401f15d6c1efff4b5f84
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-27 23:02:51 +0300

    Create Products list

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,19 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export default async function Home() {
   const billboard = await getBillboard(process.env.BILLBOARD_ID!);
+  const products = await getProducts({ isFeatured: true });
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
