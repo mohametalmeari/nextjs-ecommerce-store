4fbefdce0e91bc49f718bb0b1674538c21232334
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-01 19:33:57 +0300

    Refactor useCart hook to handle item amount

--- hooks/use-cart.ts
+++ hooks/use-cart.ts
@@ -4,19 +4,22 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 interface CartStore {
-  items: Product[];
-  addItem: (data: Product) => void;
+  items: { product: Product; amount: number }[];
+  addItem: (data: { product: Product; amount: number }) => void;
   removeItem: (id: string) => void;
   removeAll: () => void;
+  updateAmount: (id: string, amount: number) => void;
 }
 
 const useCart = create(
   persist<CartStore>(
     (set, get) => ({
       items: [],
-      addItem: (data: Product) => {
+      addItem: (data: { product: Product; amount: number }) => {
         const currentItems = get().items;
-        const existingItem = currentItems.find((item) => item.id === data.id);
+        const existingItem = currentItems.find(
+          (item) => item.product.id === data.product.id
+        );
 
         if (existingItem) {
           return toast("Item already in cart.");
@@ -27,11 +30,22 @@ const useCart = create(
       },
 
       removeItem: (id: string) => {
-        set({ items: [...get().items.filter((item) => item.id !== id)] });
+        set({
+          items: [...get().items.filter((item) => item.product.id !== id)],
+        });
         toast.success("Item removed from cart.");
       },
 
       removeAll: () => set({ items: [] }),
+      updateAmount: (id: string, amount: number) => {
+        const currentItems = get().items;
+        const item = currentItems.find((item) => item.product.id === id);
+
+        if (item) {
+          item.amount = amount;
+          set({ items: [...currentItems] });
+        }
+      },
     }),
     { name: "cart-storage", storage: createJSONStorage(() => localStorage) }
   )
