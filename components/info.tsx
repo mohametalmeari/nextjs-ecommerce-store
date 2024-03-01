e9252c29d25c6327505ae5b788e9d9bf43125826
mohametalmeari <mohamet.almeari@gmail.com>
2024-03-01 22:50:35 +0300

    [Fix bug] Fix data type of the added cart item

--- components/info.tsx
+++ components/info.tsx
@@ -7,7 +7,7 @@ import { ShoppingCart } from "lucide-react";
 import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
-  data: Product;
+  data: { product: Product; amount: number };
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
@@ -18,23 +18,23 @@ const Info: React.FC<InfoProps> = ({ data }) => {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+      <h1 className="text-3xl font-bold text-gray-900">{data.product.name}</h1>
       <div className="mt-3 flex items-end justify-between">
         <p className="text-2xl text-gray-900">
-          <Currency value={data?.price} />
+          <Currency value={data?.product?.price} />
         </p>
       </div>
       <hr className="my-4" />
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Size:</h3>
-          <div>{data?.size?.name}</div>
+          <div>{data?.product?.size?.name}</div>
         </div>
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Color:</h3>
           <div
             className="h-6 w-6 rounded-full border border-gray-600"
-            style={{ backgroundColor: data?.color?.value }}
+            style={{ backgroundColor: data?.product?.color?.value }}
           />
         </div>
       </div>
